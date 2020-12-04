2784791cd5c377804c0f9d6438a674dd08719c68
Thomson <rentongxin@sjtu.edu.cn>
2020-12-04 16:13:13 +0800

    加入 topsis代码

--- mini_book/_build/jupyter_execute/docs/evaluation_model.py
+++ mini_book/_build/jupyter_execute/docs/evaluation_model.py
@@ -486,6 +486,73 @@ fig.update_layout(
 
 fig
 
+计算的代码为
+
+import pandas as pd
+import numpy as np
+
+#极小型指标转极大型指标
+def dataDirection_1(datas, offset=0):     
+ def normalization(data):
+  return 1 / (data + offset)
+
+ return list(map(normalization, datas))
+
+#居中型指标转极大型指标
+def dataDirection_2(datas, x_min, x_max):
+ def normalization(data):
+  if data <= x_min or data >= x_max:
+   return 0
+  elif data > x_min and data < (x_min + x_max) / 2:
+   return 2 * (data - x_min) / (x_max - x_min)
+  elif data < x_max and data >= (x_min + x_max) / 2:
+   return 2 * (x_max - data) / (x_max - x_min)
+
+ return list(map(normalization, datas))
+
+#区间型指标转极大型指标
+def dataDirection_3(datas, x_min, x_max, x_minimum, x_maximum):
+ def normalization(data):
+  if data >= x_min and data <= x_max:
+   return 1
+  elif data <= x_minimum or data >= x_maximum:
+   return 0
+  elif data > x_max and data < x_maximum:
+   return 1 - (data - x_max) / (x_maximum - x_max)
+  elif data < x_min and data > x_minimum:
+   return 1 - (x_min - data) / (x_min - x_minimum)
+
+ return list(map(normalization, datas))
+
+def topsis(data, weight=None):
+ # 归一化
+ data = data / np.sqrt((data ** 2).sum())
+
+ # 最优最劣方案
+ Z = pd.DataFrame([data.min(), data.max()], index=['负理想解', '正理想解'])
+
+ # 距离
+ weight = entropyWeight(data) if weight is None else np.array(weight)
+ Result = data.copy()
+ Result['正理想解'] = np.sqrt(((data - Z.loc['正理想解']) ** 2 * weight).sum(axis=1))
+ Result['负理想解'] = np.sqrt(((data - Z.loc['负理想解']) ** 2 * weight).sum(axis=1))
+
+ # 综合得分指数
+ Result['综合得分指数'] = Result['负理想解'] / (Result['负理想解'] + Result['正理想解'])
+ Result['排序'] = Result.rank(ascending=False)['综合得分指数']
+
+ return Result, Z, weight
+
+
+data = pd.DataFrame(
+    {'人均专著': [0.1, 0.2, 0.4, 0.9, 1.2], '生师比': [5, 6, 7, 10, 2], '科研经费': [5000, 6000, 7000, 10000, 400],
+     '逾期毕业率': [4.7, 5.6, 6.7, 2.3, 1.8]}, index=['院校' + i for i in list('ABCDE')])
+
+data['生师比'] = dataDirection_3(data['生师比'], 5, 6, 2, 12)   # 师生比数据为区间型指标
+data['逾期毕业率'] = 1 / data['逾期毕业率']   # 逾期毕业率为极小型指标
+Result, Z, weight = topsis(data, weight=[0.2, 0.3, 0.4, 0.1])    # 设置权系数
+Result
+
 上面是通过举例来进行TOPSIS方法的实现，关于TOPSIS方法的更详细理论推导，参考[这里](https://wiki.mbalib.com/wiki/TOPSIS%E6%B3%95)。
 
 在实际论文撰写的过程中，最好理论性强一些，而不是像我们上面一样简单地代入数据计算。
